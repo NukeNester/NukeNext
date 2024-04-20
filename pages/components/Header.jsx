1d16337f68deae80c7b71b96ed32e20d28cfa17b
Kshitij Kochhar <kshitij@ctfguide.com>
2024-04-20 01:06:24 -0400

    added header component

---
+++ pages/components/Header.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+import Link from 'next/link';
+
+// Define the navigation links
+const navigation = [
+    { name: 'Orders', href: '/orders' },
+    { name: 'Query', href: '/query' },
+    { name: 'Settings', href: '/settings' },
+];
+
+export function Header() {
+
+    return (
+        
+            <header className=" z-50">
+
+                <nav className="flex items-center justify-between p-6 px-8" aria-label="Global">
+                    <div className="flex-1">
+                        <p className="text-xl font-semibold leading-6 text-white">
+                            Nuke Nester
+                        </p>
+                    </div>
+
+                    <div className="flex gap-x-12">
+                        {navigation.map((item) => (
+                            <a key={item.name} href={item.href} className="text-md font-semibold leading-6 text-white">
+                                {item.name}
+                            </a>
+                        ))}
+                    </div>
+                    <div className="lg:flex lg:flex-1 lg:justify-end">
+                        <a href="./api/auth/login" className="text-sm font-semibold leading-6 text-white">
+                            Log in <span aria-hidden="true">&rarr;</span>
+                        </a>
+                    </div>
+                </nav>
+                <div />
+
+
+            </header>
+
+    );
+}
+
+export default Header;
--- pages/components/Hero.jsx
+++ pages/components/Hero.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import Link from 'next/link';
+import Header from './Header';
 
 // Define the navigation links
 const navigation = [
@@ -12,33 +13,7 @@ export function Hero() {
 
     return (
         <div className="">
-            <header className=" z-50">
-
-                <nav className="flex items-center justify-between p-6 px-8" aria-label="Global">
-                    <div className="flex-1">
-                        <p className="text-xl font-semibold leading-6 text-white">
-                            Nuke Nester
-                        </p>
-                    </div>
-
-                    <div className="flex gap-x-12">
-                        {navigation.map((item) => (
-                            <a key={item.name} href={item.href} className="text-md font-semibold leading-6 text-white">
-                                {item.name}
-                            </a>
-                        ))}
-                    </div>
-                    <div className="lg:flex lg:flex-1 lg:justify-end">
-                        <a href="./api/auth/login" className="text-sm font-semibold leading-6 text-white">
-                            Log in <span aria-hidden="true">&rarr;</span>
-                        </a>
-                    </div>
-                </nav>
-                <div />
-
-
-            </header>
-
+            <Header/>
             <div className=" pt-14">
 
                 <div className="py-24 sm:py-32 lg:pb-40">
