7b9ecfc7a01c8b7d8574490f8e6124ae08e62600
Tirth Patel <tirthppatelofficial@gmail.com>
2024-04-20 19:32:21 -0400

    Merge conflict

--- pages/query.jsx
+++ pages/query.jsx
@@ -19,9 +19,28 @@ export default function Query() {
   const { redirectToLoginPage } = useRedirectFunctions();
   const [searchQuery, setSearchQuery] = useState("");
   const [map, setMap] = useState(null);
-  const [searchResult, setSearchResult] = useState(null);
-  const mapContainerRef = useRef(null);
+  const [searchResult, setSearchResult] = useState(null); // State to hold search result
+  const [suggestions, setSuggestions] = useState([]);
 
+  // Function to handle search form submission
+  const handleSearchChange = async (event) => {
+    setSearchQuery(event.target.value);
+    if (event.target.value.length > 2) {  // Only search if the query length is greater than 2 characters
+      try {
+        const response = await fetch(
+          `https://api.mapbox.com/geocoding/v5/mapbox.places/${event.target.value}.json?access_token=${mapboxgl.accessToken}&autocomplete=true`
+        );
+        const data = await response.json();
+        setSuggestions(data.features);
+      } catch (error) {
+        console.error("Error fetching suggestions:", error);
+      }
+    } else {
+      setSuggestions([]);
+    }
+  };
+
+  // Handle form submission
   const handleSearchSubmit = async (event) => {
     event.preventDefault();
     try {
@@ -81,17 +100,158 @@ export default function Query() {
 
   const renderSearchResult = () => {
     if (searchResult) {
-    //   return (
-    //     <div>
-    //       <h3>Search Result</h3>
-    //       <p>Place Name: {searchResult.placeName}</p>
-    //       <p>Latitude: {searchResult.latitude}</p>
-    //       <p>Longitude: {searchResult.longitude}</p>
-    //     </div>
-    //   );
+      return (
+        <div>
+          <h3>Search Result</h3>
+          <p>Place Name: {searchResult.placeName}</p>
+          <p>Latitude: {searchResult.latitude}</p>
+          <p>Longitude: {searchResult.longitude}</p>
+        </div>
+      );
     }
     return null;
   };
+
+  //   // useEffect to initialize the map
+  //   useEffect(() => {
+  //     if (process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN) { // Only initialize the map if it hasn't been initialized already
+
+  //       mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN; // Set the access token right before using it
+
+  //       const newMap = new mapboxgl.Map({
+  //         container: "map", // Make sure this ID matches the div ID
+  //         style: "mapbox://styles/mapbox/streets-v11",
+  //         center: [-74.006, 40.7128], // Adjust this center as needed for your use case
+  //         zoom: 9,
+  //       });
+
+  //       newMap.on('load', () => {
+  //         setMap(newMap); // When the map is loaded, then set it to the state
+  //       });
+
+  //       // If you are adding controls to the map, it should be done here
+  //       newMap.addControl(new mapboxgl.NavigationControl());
+  //     }
+
+  //   }, []);
+
+  const drawPolygon = (center, radius, key) => {
+    //TODO FIX THIS
+    // List of sources to exclude from removal
+
+    // Remove all existing layers except excluded layers
+    Object.keys(map.getStyle().sources).forEach((source) => {
+      if (source == key) {
+        const layerId = map
+          .getStyle()
+          .layers.find((layer) => layer.source === source)?.id;
+        if (layerId) {
+          map.removeLayer(layerId);
+        }
+        map.removeSource(source);
+      }
+    });
+
+    //TODO
+    //Create a query that gets all order within a circle distance of the specified location
+    //Return a list of all orders that match this query
+    //Create a map function iterating through the list, where each sourceID is is tied to the index,
+    //Create a circle based on the position for each index.
+    //Radius should be based on quantity and type?
+
+    // Generate coordinates for the circular polygon
+    const coordinates = generateCircleCoordinates(center, 0.01);
+
+    //Add the layers to the map
+    addPolygonLayer(map, key, key, coordinates);
+  };
+
+  // Add the source and layer
+  const addPolygonLayer = (map, sourceId, layerId, coordinates) => {
+    map
+      .addSource(sourceId, {
+        type: "geojson",
+        data: {
+          type: "Feature",
+          geometry: {
+            type: "Polygon",
+            coordinates: [coordinates],
+          },
+        },
+      })
+      .addLayer({
+        id: layerId,
+        type: "fill",
+        source: sourceId,
+        layout: {},
+        paint: {
+          "fill-color": "red",
+          "fill-opacity": 0.5,
+        },
+      });
+  };
+
+  // Function to generate coordinates for a circular polygon
+  const generateCircleCoordinates = (center, radius) => {
+    const coordinates = [];
+    const numSegments = 64; // Number of line segments to approximate the circle
+
+    for (let i = 0; i < numSegments; i++) {
+      const angle = (i / numSegments) * Math.PI * 2;
+      const x = center[0] + radius * Math.cos(angle);
+      const y = center[1] + radius * Math.sin(angle);
+      coordinates.push([x, y]);
+    }
+
+    // Close the polygon by repeating the first coordinate
+    coordinates.push(coordinates[0]);
+    return coordinates;
+  };
+  //const mapContainerRef = useRef(null);
+  //const [map, setMap] = useState(null);
+
+  // Function to fetch location data from the backend using Axios
+  async function fetchLocations() {
+    try {
+      const response = await Axios.get(
+        "https://server-iwh0.onrender.com/orders/getOrderByArea",
+        {
+          params: {
+            topLeft: [-79.4877, 39.722], // Specify the actual coordinates for the top left corner
+            bottomRight: [-75.0487, 37.9117],
+          },
+        }
+      );
+      return response.data; // Accessing data directly from Axios response
+    } catch (error) {
+      console.error("Failed to fetch locations", error);
+      return [];
+    }
+  }
+
+  useEffect(() => {
+    const newMap = new mapboxgl.Map({
+      container: "map",
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [-76.902221, 39.299901], // Central coordinates of the map
+      zoom: 10,
+    });
+
+    newMap.on("load", async () => {
+      setMap(newMap);
+      newMap.addControl(new mapboxgl.NavigationControl());
+
+      const locations = await fetchLocations();
+      locations.forEach((location) => {
+        new mapboxgl.Marker()
+          .setLngLat([location.longitude, location.latitude])
+          .addTo(newMap);
+      });
+    });
+
+    // Clean up on unmount
+    return () => newMap.remove();
+  }, []);
 
   return (
     <>
@@ -121,20 +281,7 @@ export default function Query() {
                 Search
               </button>
             </form>
-            {/* {renderSearchResult()} */}
-            <ul className="absolute z-10 list-none bg-white rounded shadow-lg mt-2 w-48">
-              {suggestions.map((suggestion) => (
-                <li key={suggestion.id} className="w-48 p-2 hover:bg-gray-300 cursor-pointer" onClick={() => selectSuggestion(suggestion)}>
-                  {suggestion.place_name}
-                </li>
-              ))}
-            </ul>
-            {searchResult && (
-              <div className="mt-4">
-                <h3>{searchResult.placeName}</h3>
-                <p>{searchResult.description}</p>
-              </div>
-            )}
+            {renderSearchResult()}
           </div>
         </div>
         <div ref={mapContainerRef} style={{ height: "400px", width: "75%" }} />
