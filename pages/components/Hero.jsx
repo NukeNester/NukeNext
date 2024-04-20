eb29595cfdff25659970319eae2930e4e1f3043d
Eric Chen <echen9870@gmail.com>
2024-04-20 13:53:23 -0400

    Revamped auth, revamped navbar

--- pages/components/Hero.jsx
+++ pages/components/Hero.jsx
@@ -1,45 +1,50 @@
-import { useState } from 'react';
-import Link from 'next/link';
-import Header from './Header';
+import { useState } from "react";
+import Link from "next/link";
+import Header from "./Header";
+import {
+  withAuthInfo,
+  useLogoutFunction,
+  useRedirectFunctions,
+  
+} from "@propelauth/react";
 
+export function Hero({ isLoggedIn }) {
+  const logout = useLogoutFunction();
+  const { redirectToLoginPage, redirectToAccountPage } = useRedirectFunctions();
 
-export function Hero() {
-
-    return (
-        <div className="">
-            <Header/>
-            <div className=" pt-14">
-
-                <div className="py-24 sm:py-32 lg:pb-40">
-                    <div className="mx-auto max-w-7xl px-6 lg:px-8">
-                        <div className="mx-auto max-w-4xl text-center pb-96">
-                            <h1 className="text-5xl font-bold  text-white sm:text-6xl">
-                                Nuke Nester
-                            </h1>
-                            <p className="mt-6 text-lg leading-8 text-white">
-                                Properly dispose and handle nuclear waste with Nuke Nester.
-                            </p>
-                            <div className="mt-10 flex items-center justify-center gap-x-6 pb-20">
-                                <a
-                                    href="./api/auth/login"
-                                    className="rounded-md px-6 py-1.5 text-lg font-semibold text-white border border-white hover:bg-slate-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-400"
-                                >
-                                    Get started
-                                </a>
-                                <a
-                                    href="./api/auth/login"
-                                    className="rounded-md px-6 py-1.5 text-lg font-semibold text-white border border-white hover:bg-slate-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-400"
-                                >
-                                    Already a User?
-                                </a>
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
+  return (
+    <>
+      <Header isLoggedIn={isLoggedIn} handleLogIn = {redirectToLoginPage} handleLogOut={logout} />
+      <div className="pt-14">
+        <div className="py-24 sm:py-32 lg:pb-40">
+          <div className="mx-auto max-w-7xl px-6 lg:px-8">
+            <div className="mx-auto max-w-4xl text-center pb-96">
+              <h1 className="text-5xl font-bold text-white sm:text-6xl">
+                Nuke Nester
+              </h1>
+              <p className="mt-6 text-lg leading-8 text-white">
+                Properly dispose and handle nuclear waste with Nuke Nester.
+              </p>
+              <div className="mt-10 flex items-center justify-center gap-x-6 pb-20">
+                <a
+                  onClick={() => redirectToLoginPage()}
+                  className="rounded-md px-6 py-1.5 text-lg font-semibold text-white border border-white hover:bg-slate-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-400"
+                >
+                  Get started
+                </a>
+                <a
+                  onClick={() => redirectToLoginPage()}
+                  className="rounded-md px-6 py-1.5 text-lg font-semibold text-white border border-white hover:bg-slate-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-400"
+                >
+                  Already a User?
+                </a>
+              </div>
             </div>
+          </div>
         </div>
-    );
+      </div>
+    </>
+  );
 }
 
 export default Hero;
--- pages/statistics.jsx
+++ pages/statistics.jsx
@@ -1,35 +1,49 @@
 // pages/Statistics.jsx
-import Head from 'next/head';
-import WasteStats from './components/WasteStats'; // Ensure the import paths are correct
-import EmissionsSaved from './components/EmissionsSaved'; // Ensure the import paths are correct
-import LineGraph from './components/LineGraph'; // Ensure the import paths are correct
-import HomeButton from './components/HomeButton'; // Ensure the import paths are correct
-import BarGraph from './components/BarGraph';
-import Header from './components/Header';
-export default function Statistics() {
-    return (
-        <>
-            <Header />
-            <div className="relative min-h-screen bg-gradient-to-b from-[#86c6aa] to-gray-200 p-4">
+import Head from "next/head";
+import WasteStats from "./components/WasteStats"; // Ensure the import paths are correct
+import EmissionsSaved from "./components/EmissionsSaved"; // Ensure the import paths are correct
+import LineGraph from "./components/LineGraph"; // Ensure the import paths are correct
+import HomeButton from "./components/HomeButton"; // Ensure the import paths are correct
+import BarGraph from "./components/BarGraph";
+import Header from "./components/Header";
+import React, { useRef } from "react";
+import {
+  withAuthInfo,
+  useLogoutFunction,
+  useRedirectFunctions,
+  useAuthInfo,
+} from "@propelauth/react";
 
-                {/* Central content */}
-                <div className="flex flex-col items-center justify-center w-full">
-                    <h1 className="text-3xl font-bold text-white mb-4">Profile Statistics</h1>
-                    <p className="text-white mb-6">View your profile statistics</p>
-                    {/* Stats Container */}
-                    <div className="flex flex-wrap justify-center gap-4 mb-6">
-                        <WasteStats />
-                        <EmissionsSaved />
-                    </div>
-                
-                            <LineGraph />
-                            
-                    
-                           
-                </div>
-                
-                    
-            </div>
-        </>
-    );
+export default function Statistics() {
+  const { loading, isLoggedIn, user } = useAuthInfo();
+  const logout = useLogoutFunction();
+  const { redirectToLoginPage, redirectToAccountPage } = useRedirectFunctions();
+  const myRef = useRef(null);
+  const executeScroll = () =>
+    myRef.current.scrollIntoView({ behavior: "smooth" });
+  return (
+    <>
+      <Header
+        isLoggedIn={isLoggedIn}
+        handleLogIn={redirectToLoginPage}
+        handleLogOut={logout}
+      />
+      <div className="relative min-h-screen bg-gradient-to-b from-[#86c6aa] to-gray-200 p-4">
+        {/* Central content */}
+        <div className="flex flex-col items-center justify-center w-full">
+          <h1 className="text-3xl font-bold text-white mb-4">
+            Profile Statistics
+          </h1>
+          <p className="text-white mb-6">View your profile statistics</p>
+          {/* Stats Container */}
+          <div className="flex flex-wrap justify-center gap-4 mb-6">
+            <WasteStats />
+            <EmissionsSaved />
+          </div>
+
+          <LineGraph />
+        </div>
+      </div>
+    </>
+  );
 }
