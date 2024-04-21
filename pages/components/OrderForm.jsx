7c54b6b2a7978b87014f97e9cada0b1445add121
Kshitij Kochhar <kshitij@ctfguide.com>
2024-04-20 22:14:58 -0400

    Made OrderForm look pretty :D

--- pages/components/OrderForm.jsx
+++ pages/components/OrderForm.jsx
@@ -14,7 +14,8 @@ const OrderForm = ({ organizationName, handlePopupClose }) => {
     latitude: 0,
     longitude: 0,
   });
-  const [marker, setMarker] = useState(null);
+  //const [marker, setMarker] = useState(null);
+  let marker = null;
 
   useEffect(() => {
     const newMap = new mapboxgl.Map({
@@ -37,10 +38,10 @@ const OrderForm = ({ organizationName, handlePopupClose }) => {
         }
 
         // Create a new marker at the clicked location
-        const newMarker = new mapboxgl.Marker()
+        marker = new mapboxgl.Marker()
           .setLngLat([lngLat.lng, lngLat.lat])
           .addTo(newMap);
-        setMarker(newMarker);
+
 
         // Update order data with the clicked location
         setOrderData((prevOrderData) => ({
@@ -62,7 +63,7 @@ const OrderForm = ({ organizationName, handlePopupClose }) => {
 
   const handleSubmit = async () => {
     try {
-        console.log(orderData);
+      console.log(orderData);
       const response = await axios.post(
         `https://server-iwh0.onrender.com/orders/createOrder`,
         orderData
@@ -82,39 +83,44 @@ const OrderForm = ({ organizationName, handlePopupClose }) => {
         fontFamily: "Poppins, sans-serif",
         backgroundColor: "#161716",
       }}
-      className="max-w-7xl relative inline-block align-bottom w-5/6 pb-10 pt-10 bg-gray-900 border border-gray-700 rounded-lg px-20 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle "
+      className="max-w-7xl relative inline-block align-bottom w-5/6 py-10 bg-gray-900 border border-gray-700 rounded-lg px-20 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle "
     >
       <div>
         <div className="mt-3 sm:mt-5">
           <h1 className="text-white text-5xl text-center pb-10">
             Place An Order
           </h1>
-          <div className="flex justify-center items-center ">
+          <div className="flex justify-center items-center gap-x-20">
+
             {/* INPUT BOX */}
-            <div className="text-white text-lg pb-12 px-10 ">
+            <div className="text-white text-lg pb-2 px-10">
               {/* Prompt 1 */}
               <div className="flex items-start flex-col mb-10">
-                <label className=" text-white pb-1">Type of Waste:</label>
-                <input
-                  type="text"
-                  name="wasteType"
-                  value={orderData.wasteType}
-                  onChange={handleInputChange}
-                  className={styles.buttonBrownStyle}
-                />
+                <label className=" text-white pb-1 ">Type of Waste:</label>
+                <select onChange={handleInputChange} id="wasteType" name="wasteType" className="border border-neutral-700 px-16 rounded-md text-white py-2 bg-neutral-800 hover:text-neutral-300 hover:bg-neutral-800/90" >
+                  <option value="Uranium">Uranium</option>
+                  <option value="Plutonium">Plutonium</option>
+                  <option value="Thorium">Thorium</option>
+                  <option value="Americium">Americium</option>
+                </select>
               </div>
+
+
+
 
               {/* Prompt 2 */}
               <div className="flex items-start flex-col mb-10">
-                <label className=" text-white pb-1">Quantity of Waste:</label>
+                <label className=" text-white pb-1">Quantity of Waste {"(lbs)"}:</label>
                 <input
-                  type="text"
+                  type="number"
                   name="quantity"
                   value={orderData.quantity}
                   onChange={handleInputChange}
-                  className={styles.buttonBrownStyle}
+                  className={styles.inputBoxStyle}
                 />
               </div>
+
+
 
               {/* Prompt 3 */}
               <div className="flex items-start flex-col mb-10">
@@ -124,14 +130,20 @@ const OrderForm = ({ organizationName, handlePopupClose }) => {
                   name="timeToDecay"
                   value={orderData.timeToDecay}
                   onChange={handleInputChange}
-                  className={styles.buttonBrownStyle}
+                  className="border border-neutral-700 px-11 rounded-md text-white py-2 bg-neutral-800 hover:text-neutral-300 hover:bg-neutral-800/90"
                 />
               </div>
+
             </div>
+
             {/* Map Display */}
-            <div id="map" style={{ height: "400px", width: "75%" }} />
+            <div className="" id="map" style={{ height: "400px", width: "50%" }} />
+
+
           </div>
-          <div className=" py-5 ">
+
+
+          <div className="pt-20 ">
             <div className="flex items-center justify-center">
               <button
                 className={styles.buttonBrownStyle}
@@ -158,6 +170,8 @@ const styles = {
     "border border-neutral-700 mx-3 rounded-md w-20 text-white py-2 bg-neutral-800 hover:text-neutral-300 hover:bg-neutral-800/90",
   buttonGreenStyle:
     "border border-neutral-700 mx-3 rounded-md px-4 text-white py-2 bg-green-700 hover:text-neutral-300 hover:bg-green-800",
+  inputBoxStyle:
+    "border border-neutral-700 px-6 rounded-md text-white py-2 bg-neutral-800 hover:text-neutral-300 hover:bg-neutral-800/90",
 };
 
 export default OrderForm;
